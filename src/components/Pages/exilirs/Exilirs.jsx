35ae6dd64e242b0886f90136ad073aa6704e0e51
Joaquín Uriel García <82168815+JoaquinDev542@users.noreply.github.com>
2023-06-03 13:40:03 +0200

    changes

--- src/components/Pages/exilirs/Exilirs.jsx
+++ src/components/Pages/exilirs/Exilirs.jsx
@@ -13,8 +13,8 @@ export const Exilirs = () => {
         <div className="Exilirs">
             <TitleContent textH2={ h2 } textP={ p }/>    
             <div className="Exilirs-container">
-                <CardProduct href="/Collections/Immnunity-Boost" src=".././assets/products/immunity-boost.webp" number="From $10" title="Immunity Boost" btn="Add to Cart"/>
-                <CardProduct href="/Collections/Tumeric-Juice" src=".././assets/products/tumeric-juice.webp" number="$7.00" title="Tumeric Juice" btn="Add to Cart"/>
+                <CardProduct href="/Collections/Immnunity-Boost" src="https://joaquindev542.github.io/assets/products/immunity-boost.webp" number="From $10" title="Immunity Boost" btn="Add to Cart"/>
+                <CardProduct href="/Collections/Tumeric-Juice" src="https://joaquindev542.github.io/assets/products/tumeric-juice.webp" number="$7.00" title="Tumeric Juice" btn="Add to Cart"/>
             </div>
         </div>
     )
