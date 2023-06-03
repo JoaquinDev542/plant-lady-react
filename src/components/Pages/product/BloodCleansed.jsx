4c95df5189a60e4b7eb22847efcad189bbaec8aa
Joaquín Uriel García <82168815+JoaquinDev542@users.noreply.github.com>
2023-06-03 15:54:53 +0200

    changes

--- src/components/Pages/product/BloodCleansed.jsx
+++ src/components/Pages/product/BloodCleansed.jsx
@@ -2,6 +2,6 @@ import { ProductInformation } from "../../productInformation/ProductInformation"
 
 export const BloodCleansed = () => {
     return (
-        <ProductInformation img="./assets/products/blood-cleansed.webp" title="Blood Cleansed" price="$12.00" subtitle="Can't beet it!" secondP="This is a blood-cleansing pure beet juice, with a kick of sweet spice." thirdP="The doctrine of signatures says red plants are great for your circulatory system. Drink this juice to detox your blood and gain more clarity." fourthP="Before drinking, shake the bottle and visualize it giving you just what your body, mind, and spirit need." fifthP="beet, dandelion greens, apple, lemon, ginger"/>
+        <ProductInformation img=".././assets/products/blood-cleansed.webp" title="Blood Cleansed" price="$12.00" subtitle="Can't beet it!" secondP="This is a blood-cleansing pure beet juice, with a kick of sweet spice." thirdP="The doctrine of signatures says red plants are great for your circulatory system. Drink this juice to detox your blood and gain more clarity." fourthP="Before drinking, shake the bottle and visualize it giving you just what your body, mind, and spirit need." fifthP="beet, dandelion greens, apple, lemon, ginger"/>
     )
 }
